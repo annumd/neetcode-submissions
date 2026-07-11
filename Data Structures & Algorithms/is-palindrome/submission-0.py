44bf267566317dbf22551e800b4b8a1a56f2dfa4
annumd <161940207+annumd@users.noreply.github.com>
2026-07-11 08:19:39 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        word=""
+        for ch in s:
+            if ch.isalnum():
+                word+=ch.lower()
+            else:
+                continue
+        pal=word[::-1]
+        if word==pal:
+            return True
+        else:
+            return False
